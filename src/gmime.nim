ec748b1b020cf82cdccd72fbe4d2a532269f9995
George Lemon <georgelemon@protonmail.com>
2026-02-22 20:57:06 +0200

    fix linking
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- src/gmime.nim
+++ src/gmime.nim
@@ -8,9 +8,9 @@ when defined(macosx):
   {.passC:"-I /opt/local/include -I /opt/local/include/gmime-3.0 -I/opt/local/include/glib-2.0/ -I/opt/local/lib/glib-2.0/include".}
   {.passL:"-L/opt/local/lib -lgmime-3.0 -lgobject-2.0 -lglib-2.0 -lgmodule-2.0".}
 elif defined(linux):
-  --passC:"-I /usr/include -I /usr/include/gmime-3.0 -I/usr/include/glib-2.0/ -I/usr/lib/x86_64-linux-gnu/glib-2.0/include"
-  --passL:"-L/usr/lib -lgmime-3.0 -lgobject-2.0 -lglib-2.0 -lgmodule-2.0"
-  --passL:"-L/usr/lib/x86_64-linux-gnu -lgmime-3.0 -lgobject-2.0 -lglib-2.0 -lgmodule-2.0"
+  {.passC:"-I /usr/include -I /usr/include/gmime-3.0 -I/usr/include/glib-2.0/ -I/usr/lib/x86_64-linux-gnu/glib-2.0/include".}
+  {.passL:"-L/usr/lib -lgmime-3.0 -lgobject-2.0 -lglib-2.0 -lgmodule-2.0".}
+  {.passL:"-L/usr/lib/x86_64-linux-gnu -lgmime-3.0 -lgobject-2.0 -lglib-2.0 -lgmodule-2.0".}
 else:
   {.error: "Unsupported platform. Please add the appropriate include and library paths for your system." .}
 
