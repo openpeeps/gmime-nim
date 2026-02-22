5f2cd41d20f5eb1e8d46518b657ce8d38ab3b3be
George Lemon <georgelemon@protonmail.com>
2026-02-22 20:51:20 +0200

    update
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- src/gmime.nim
+++ src/gmime.nim
@@ -4,10 +4,15 @@
 #          Made by Humans from OpenPeeps
 #          https://github.com/openpeeps/gmime-nim
 
-{.
-  passC:"-I /opt/local/include -I /opt/local/include/gmime-3.0 -I/opt/local/include/glib-2.0/ -I/opt/local/lib/glib-2.0/include"
-  passL:"-L/opt/local/lib -lgmime-3.0 -lgobject-2.0 -lglib-2.0 -lgmodule-2.0",
-.}
+when defined(macosx):
+  {.passC:"-I /opt/local/include -I /opt/local/include/gmime-3.0 -I/opt/local/include/glib-2.0/ -I/opt/local/lib/glib-2.0/include".}
+  {.passL:"-L/opt/local/lib -lgmime-3.0 -lgobject-2.0 -lglib-2.0 -lgmodule-2.0".}
+elif defined(linux):
+  {.passC:"-I /usr/include -I /usr/include/gmime-3.0 -I/usr/include/glib-2.0/ -I/usr/lib/glib-2.0/include".}
+  {.passL:"-L/usr/lib -lgmime-3.0 -lgobject-2.0 -lglib-2.0 -lgmodule-2.0".}
+  {.passL:"-L/usr/lib/x86_64-linux-gnu -lgmime-3.0 -lgobject-2.0 -lglib-2.0 -lgmodule-2.0".}
+else:
+  {.error: "Unsupported platform. Please add the appropriate include and library paths for your system." .}
 
 import ./gmime/bindings/gmime_bindings
 export gmime_bindings
--- tests/config.nims
+++ tests/config.nims
@@ -1,1 +1,9 @@
 switch("path", "$projectDir/../src")
+
+when defined(macosx):
+  --passC:"-I /opt/local/include -I /opt/local/include/gmime-3.0 -I/opt/local/include/glib-2.0/ -I/opt/local/lib/glib-2.0/include"
+  --passL:"-L/opt/local/lib -lgmime-3.0 -lgobject-2.0 -lglib-2.0 -lgmodule-2.0"
+elif defined(linux):
+  --passC:"-I /usr/include -I /usr/include/gmime-3.0 -I/usr/include/glib-2.0/ -I/usr/lib/glib-2.0/include"
+  --passL:"-L/usr/lib -lgmime-3.0 -lgobject-2.0 -lglib-2.0 -lgmodule-2.0"
+  --passL:"-L/usr/lib/x86_64-linux-gnu -lgmime-3.0 -lgobject-2.0 -lglib-2.0 -lgmodule-2.0"
